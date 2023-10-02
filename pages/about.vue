ae334c82ebb0aea95ba6d8eb8e08d28e2f3e7d79
lewis <thiengat1@gmail.com>
2023-10-02 10:07:06 +0700

    init project

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>About</h2>
+  <p class="">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates libero,
+    ex, ducimus aspernatur cumque voluptatem suscipit nesciunt officiis,
+    deleniti dicta illo? Nemo atque eligendi, alias eveniet nesciunt voluptas
+    deserunt. Vero!
+  </p>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+      libero, ex, ducimus aspernatur cumque voluptatem suscipit nesciunt
+      officiis, deleniti dicta illo? Nemo atque eligendi, alias eveniet nesciunt
+      voluptas deserunt. Vero!
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
